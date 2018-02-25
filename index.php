2d17bdc1c2c73a7edb094c8214d1e70a5b3a8300
Mycroft1891 <mycroft1891@gmail.com>
2018-02-25 19:47:48 +0100

    remove docs folder

--- index.php
+++ index.php
@@ -5,7 +5,28 @@
     <title>Title</title>
   </head>
   <body>
-    <?php echo 'Hello World' ?>
+    <?php
 
+    $my_name = 'alexander';
+    $arr = ['a', 'b', 'c'];
+    // $arr = (int) $arr;
+
+    if (is_int($arr)) {
+      echo "this array is an integer: $arr";
+    } else {
+      echo 'this array is not an integer';
+    }
+
+    $large_number = 9223372036854775807;
+    var_dump($large_number);
+
+    for ($i=0; $i < count($arr); $i++) {
+      echo "array: $arr[$i]\n";
+    }
+
+    foreach ($arr as $char) {
+      echo $char;
+    }
+    ?>
   </body>
 </html>
