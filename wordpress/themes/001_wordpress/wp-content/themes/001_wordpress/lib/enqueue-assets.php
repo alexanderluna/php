dea135a8ef29fd6cfaa73006755639a147d1ab64
Mycroft1891 <mycroft1891@gmail.com>
2018-12-26 13:38:22 +0100

    Add: sass assets and import files using PHP

---
+++ wordpress/themes/001_wordpress/wp-content/themes/001_wordpress/lib/enqueue-assets.php
@@ -0,0 +1,12 @@
+<?php
+function firsttheme_assets() {
+	wp_enqueue_style(
+		'firsttheme-stylesheet',
+		get_template_directory_uri() . '/dist/assets/css/bundle.css',
+		array(),
+		'all'
+	);
+}
+
+add_action('wp_enqueue_scripts', 'firsttheme_assets');
+
